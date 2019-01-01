54a13a2a26258224dc1ef8d200b9bf5fdd732270
Josh Komoroske <jdkomo@gmail.com>
2019-01-01 01:47:24 -0800

    Generate docs for Terraform module (#4)

---
+++ module/README.md
@@ -0,0 +1,12 @@
+# Terraform GKE Kubeconfig
+
+📝 Terraform module that creates a Kubeconfig for accessing GKE clusters
+
+## Usage:
+
+```hcl
+module "kubeconfig" {
+  source = "github.com/joshdk/terraform-gke-kubeconfig//module"
+}
+```
+
--- module/main.tf
+++ module/main.tf
@@ -1,1 +1,14 @@
+/**
+ * # Terraform GKE Kubeconfig
+ *
+ * 📝 Terraform module that creates a Kubeconfig for accessing GKE clusters
+ *
+ * ## Usage:
+ *
+ * ```hcl
+ * module "kubeconfig" {
+ *   source = "github.com/joshdk/terraform-gke-kubeconfig//module"
+ * }
+ * ```
+ */
 
