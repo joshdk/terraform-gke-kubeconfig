02dcb07c66ebb346381b5f58b93e1c216ef0db0a
Josh Komoroske <jdkomo@gmail.com>
2019-01-01 21:46:04 -0800

    Data source for finding GKE cluster by name (#6)

--- module/main.tf
+++ module/main.tf
@@ -6,9 +6,20 @@
  * ## Usage:
  *
  * ```hcl
+ * provider "google" {
+ *   ...
+ * }
+ *
  * module "kubeconfig" {
  *   source = "github.com/joshdk/terraform-gke-kubeconfig//module"
+ *   name   = "my-cluster"
+ *   zone   = "us-east1-a"
  * }
  * ```
  */
 
+data "google_container_cluster" "cluster" {
+  name   = "${var.name}"
+  region = "${var.region}"
+  zone   = "${var.zone}"
+}
---
+++ module/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  type        = "string"
+  description = "The name of the cluster."
+}
+
+variable "region" {
+  type        = "string"
+  default     = ""
+  description = "The region this cluster has been created in."
+}
+
+variable "zone" {
+  type        = "string"
+  default     = ""
+  description = "The zone this cluster has been created in."
+}
